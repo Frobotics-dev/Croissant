6c28619b22cd4febd095bd7d3423982e9a278187
Frederik Mondel <frobotics@freenet.de>
2025-10-17 00:20:30 +0200

    Initial Commit

---
+++ Croissant/CroissantApp.swift
@@ -0,0 +1,17 @@
+//
+//  CroissantApp.swift
+//  Croissant
+//
+//  Created by Frederik Mondel on 17.10.25.
+//
+
+import SwiftUI
+
+@main
+struct CroissantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
